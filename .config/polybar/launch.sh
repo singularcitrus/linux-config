ab1d349ab6c8a74b3b1e2aea05b0d6880b662949
Lourens <lourensros@gmail.com>
2021-11-22 10:54:04 +0200

    polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "Bars launched..."
